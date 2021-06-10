7f97ad6a21465b65ed404cd78180044fd3acd653
SureshPalakoora <suresh.palakoora@gmail.com>
2021-06-10 11:23:25 +0530

    Third line

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ print("hello")
 print("Welcome to Python")
 print("Programming")
 print("Adding other lines of code")
+print("Third line")
 print("Second Line")
 print("Adding other lines for other branch")
 
